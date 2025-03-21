16a5e6521901c1aec36d10ddb0d982e71c4013af
Robobo711 <80320790+Robobo711@users.noreply.github.com>
2025-03-21 18:03:25 +0800

    script.js

--- door_lock/script.js
+++ door_lock/script.js
@@ -1,4 +1,4 @@
-// 儲存 cookie
+// 儲存 cookie(cookie的名字、cookie的值、儲存天數)
 function setCookie(cname, cvalue, exdays) {
   let d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000 + 8 * 60 * 60 * 1000));
@@ -23,44 +23,31 @@ const inputtext = document.getElementById('inputtext'); // Adafruit IO Key
 const inputtextUser = document.getElementById('inputtextUser'); // Adafruit IO Username
 const mask = document.getElementById('mask');
 const loadImg = document.getElementById('loadImg');
-const startBtn = document.getElementById('startRecognition');
 
 // 檢查並更新 Cookie
 function checkCookie() {
   let key = getCookie("key");
   let name = getCookie("name");
-  if (key) inputtext.value = key;
-  if (name) inputtextUser.value = name;
+  if (key != "") inputtext.value = key;
+  if (name != "") inputtextUser.value = name;
 
-  if (inputtext.value !== key) setCookie("key", inputtext.value, 30);
-  if (inputtextUser.value !== name) setCookie("name", inputtextUser.value, 30);
+  key = inputtext.value;
+  name = inputtextUser.value;
+
+  if (key != getCookie("key")) setCookie("key", key, 30);
+  if (name != getCookie("name")) setCookie("name", name, 30);
 }
 
 // 取得人名列表
-let labelStr = getCookie("labelStr") || "black,robobo";
+let labelStr = getCookie("labelStr");
+if (labelStr == "") labelStr = "Teddy,Chuan";
 labelStr = prompt("請輸入名稱並以逗號隔開人名:", labelStr);
-let labels = labelStr.split(",");
+let labels = labelStr.toString().split(",");
 
-// 圓角輸入框
+// 圓角輸入框 (需要 jQuery UI)
 $('input:text').addClass("ui-widget ui-widget-content ui-corner-all ui-textfield");
 
-// MQTT 客戶端設置（使用 Adafruit IO MQTT）
-const mqttClient = mqtt.connect('wss://io.adafruit.com:443/mqtt', {
-  username: inputtextUser.value,
-  password: inputtext.value,
-  clientId: 'web_client_' + Math.random().toString(16).substr(2, 8)
-});
-
-mqttClient.on('connect', () => console.log('MQTT 連接成功'));
-mqttClient.on('error', (err) => console.log('MQTT 錯誤:', err));
-
-// 載入模型
-let labeledDescriptors;
-let faceMatcher;
-let canvas;
-let displaySize;
-let recognitionInterval;
-
+// 載入模型並啟動攝影機
 Promise.all([
   mask.style.display = "block",
   loadImg.style.display = "block",
@@ -69,33 +56,44 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
   console.log("模型載入成功"),
   checkCookie()
-]).then(() => {
-  mask.style.display = "none";
-  loadImg.style.display = "none";
-  startBtn.addEventListener('click', startRecognition);
-});
+]).then(startVideo);
 
-async function startRecognition() {
-  // 啟動攝影機
-  const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
-  video1.srcObject = stream;
+async function startVideo() {
+  await navigator.mediaDevices.getUserMedia({ video: {} })
+    .then(function (stream) {
+      video1.srcObject = stream;
+    });
   await video1.play();
+  recognizeFacesContinuously();
+}
 
-  // 初始化人臉辨識
-  if (!labeledDescriptors) {
-    labeledDescriptors = await loadLabel();
-    faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.7);
-  }
+let labeledDescriptors;
+let faceMatcher;
+let canvas;
+let displaySize;
+
+async function recognizeFacesContinuously() {
+  // 初始化人臉描述
+  labeledDescriptors = await loadLabel();
+  faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.7);
   canvas = faceapi.createCanvasFromMedia(video1);
   document.body.append(canvas);
+  mask.style.display = "none";
+  loadImg.style.display = "none";
+
+  // 設定 Canvas 位置與大小
   canvas.style.left = getPosition(video1)["x"] + "px";
   canvas.style.top = getPosition(video1)["y"] + "px";
   displaySize = { width: video1.offsetWidth, height: video1.offsetHeight };
   faceapi.matchDimensions(canvas, displaySize);
 
-  // 開始持續辨識
-  let latestResult = "unknown";
-  recognitionInterval = setInterval(async () => {
+  // 持續辨識
+  let lastUpload = new Date().getTime() - 2000; // 控制上傳頻率
+  setInterval(async () => {
+    inputtext.style.width = video1.offsetWidth.toString() + "px";
+    inputtext.style.height = (video1.offsetHeight / 8).toString() + "px";
+    inputtextUser.style.width = video1.offsetWidth.toString() + "px";
+    inputtextUser.style.height = (video1.offsetHeight / 8).toString() + "px";
     displaySize = { width: video1.offsetWidth, height: video1.offsetHeight };
     faceapi.matchDimensions(canvas, displaySize);
 
@@ -110,33 +108,22 @@ async function startRecognition() {
       const box = resizedDetections[i].detection.box;
       const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
       drawBox.draw(canvas);
-      latestResult = result.label; // 更新最新辨識結果
+
+      const currentTime = new Date().getTime();
+      if (currentTime - lastUpload >= 10000) { // 每 10 秒上傳一次
+        $.ajax({
+          url: `https://io.adafruit.com/api/v2/${inputtextUser.value}/feeds/face/data?X-AIO-Key=${inputtext.value}`,
+          type: "POST",
+          data: { "value": result.label },
+          success: () => console.log(`Uploaded ${result.label} to Adafruit IO`),
+          error: (err) => console.log("Upload failed:", err)
+        });
+        lastUpload = currentTime;
+      }
     });
-  }, 100);
 
-  // 5 秒後上傳並停止
-  setTimeout(() => {
-    stopRecognition();
-    if (inputtextUser.value && inputtext.value) {
-      const topic = `${inputtextUser.value}/feeds/face`;
-      mqttClient.publish(topic, latestResult, {}, (err) => {
-        if (!err) console.log(`MQTT 上傳 ${latestResult} 至 ${topic}`);
-        else console.log("MQTT 上傳失敗:", err);
-      });
-    }
-  }, 5000);
-}
-
-function stopRecognition() {
-  clearInterval(recognitionInterval);
-  if (video1.srcObject) {
-    video1.srcObject.getTracks().forEach(track => track.stop());
-    video1.srcObject = null;
-  }
-  if (canvas) {
-    canvas.remove();
-    canvas = null;
-  }
+    checkCookie();
+  }, 1000); // 每 1000ms 檢查一次
 }
 
 async function loadLabel() {
@@ -159,6 +146,7 @@ async function loadLabel() {
   );
 }
 
+// 取得元素位置
 function getPosition(element) {
   let x = 0, y = 0;
   while (element) {
